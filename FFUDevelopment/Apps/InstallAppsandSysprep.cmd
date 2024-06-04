d552c3604709cddede105528692e4d1bd4984f02
Mike Kelly <167896478+MKellyCBSD@users.noreply.github.com>
2024-06-03 22:01:36 -0400

    Update InstallAppsandSysprep.cmd

--- FFUDevelopment/Apps/InstallAppsandSysprep.cmd
+++ FFUDevelopment/Apps/InstallAppsandSysprep.cmd
@@ -1,12 +1,17 @@
 REM Put each app install on a separate line
 REM M365 Apps/Office ProPlus
-REM d:\Office\setup.exe /configure d:\office\DeployFFU.xml
+d:\Office\setup.exe /configure d:\Office\DeployFFU.xml
 REM Install Defender Platform Update
 REM Install Defender Definitions
 REM Install Windows Security Platform Update
 REM Install OneDrive Per Machine
 REM Install Edge Stable
-REM Add additional apps below here
+REM Install Xaml
+REM Install VCLibsDesktop
+REM Install DesktopAppInstaller
+REM Install VCx86
+REM Install VCx64
+REM Install Teams
 REM Contoso App (Example)
 REM msiexec /i d:\Contoso\setup.msi /qn /norestart
 REM The below lines will remove the unattend.xml that gets the machine into audit mode. If not removed, the OS will get stuck booting to audit mode each time.
@@ -15,8 +20,22 @@ del c:\windows\panther\unattend\unattend.xml /F /Q
 del c:\windows\panther\unattend.xml /F /Q
 taskkill /IM sysprep.exe
 timeout /t 10
-REM Run Component Cleanup since dism /online /cleanup-image /analyzecomponentcleanup recommends it
-REM If adding latest CU, definitely need to do this to keep FFU size smaller
-dism /online /cleanup-image /startcomponentcleanup /resetbase
+REM Run disk cleanup (cleanmgr.exe) with all options enabled: https://learn.microsoft.com/en-us/troubleshoot/windows-server/backup-and-storage/automating-disk-cleanup-tool
+set rootkey=HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches
+REM Per above doc, the Offline Pages Files subkey does not have stateflags value
+for /f "tokens=*" %%K in ('reg query "%rootkey%"') do (
+    echo %%K | findstr /i /c:"Offline Pages Files"
+    if errorlevel 1 (
+        reg add "%%K" /v StateFlags0000 /t REG_DWORD /d 2 /f
+    )
+)
+cleanmgr.exe /sagerun:0
+REM Remove the StateFlags0000 registry value
+for /f "tokens=*" %%K in ('reg query "%rootkey%"') do (
+    echo %%K | findstr /i /c:"Offline Pages Files"
+    if errorlevel 1 (
+        reg delete "%%K" /v StateFlags0000 /f
+    )
+)
 REM Sysprep/Generalize
 c:\windows\system32\sysprep\sysprep.exe /quiet /generalize /oobe
