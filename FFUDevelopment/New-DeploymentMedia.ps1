a5eba498049b85d071d904bbc755f14fa76bd6d4
Mike Kelly <167896478+MKellyCBSD@users.noreply.github.com>
2024-07-02 16:29:26 -0400

    Update New-DeploymentMedia.ps1
    Update code.

--- FFUDevelopment/New-DeploymentMedia.ps1
+++ FFUDevelopment/New-DeploymentMedia.ps1
@@ -1,184 +1,246 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 param(
-    [Parameter(Mandatory = $false, Position = 0)]
-    [ValidateScript({ Test-Path $_ })]
-    [String]$FFUDevelopmentPath = "C:\FFUDevelopment",
-    [String]$DeployISO,
-    [String]$DeployISOPath
-    )
+    [Parameter(Mandatory = $True, Position = 0)]
+    $DeployISOPath
+)
+$Host.UI.RawUI.WindowTitle = 'Imaging Tool USB Creator | 2024.6.1'
+#will partition and format USB drives, copy the captured FFU's and drivers to the USB drives. If you'd like to customize the drive to add drivers, provisioning packages, name prefix, etc. You'll need to do that afterward.
 
-
-#Set default values for variables that depend on other parameters
-
-if (-not $LogFile) { $LogFile = "$FFUDevelopmentPath\FFUDevelopment.log" }
-if (-not $adkPath) { $adkPath = "C:\Program Files (x86)\Windows Kits\10\" }
-if (-not $Date) {$Date = get-date -UFormat %m%d%Y }
-if (-not $DeployISO) { $DeployISO = "WinPE_FFU_Deploy-$Date.iso"}
-if (-not $WindowsLang) { $WindowsLang = "En-US"}
-if (-not $DeployISOPath) { $DeployISOPath = "$FFUDevelopmentPath\$DeployISO" }
-
+if($DeployISOPath){
+$DevelopmentPath = $DeployISOPath | Split-Path
+$ImagesPath = "$DevelopmentPath\Images"
 function WriteLog($LogText) { 
+$LogFileName = '\Script.log'
+$LogFile = $DevelopmentPath + $LogFilename
     Add-Content -path $LogFile -value "$((Get-Date).ToString()) $LogText" -Force -ErrorAction SilentlyContinue
     Write-Verbose $LogText
 }
 
-function Invoke-Process {
-    [CmdletBinding(SupportsShouldProcess)]
-    param
-    (
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        [string]$FilePath,
-
-        [Parameter()]
-        [ValidateNotNullOrEmpty()]
-        [string]$ArgumentList
-    )
-
-    $ErrorActionPreference = 'Stop'
-
-    try {
-        $stdOutTempFile = "$env:TEMP\$((New-Guid).Guid)"
-        $stdErrTempFile = "$env:TEMP\$((New-Guid).Guid)"
-
-        $startProcessParams = @{
-            FilePath               = $FilePath
-            ArgumentList           = $ArgumentList
-            RedirectStandardError  = $stdErrTempFile
-            RedirectStandardOutput = $stdOutTempFile
-            Wait                   = $true;
-            PassThru               = $true;
-            NoNewWindow            = $true;
-        }
-        if ($PSCmdlet.ShouldProcess("Process [$($FilePath)]", "Run with args: [$($ArgumentList)]")) {
-            $cmd = Start-Process @startProcessParams
-            $cmdOutput = Get-Content -Path $stdOutTempFile -Raw
-            $cmdError = Get-Content -Path $stdErrTempFile -Raw
-            if ($cmd.ExitCode -ne 0) {
-                if ($cmdError) {
-                    throw $cmdError.Trim()
-                }
-                if ($cmdOutput) {
-                    throw $cmdOutput.Trim()
-                }
-            }
-            else {
-                if ([string]::IsNullOrEmpty($cmdOutput) -eq $false) {
-                    WriteLog $cmdOutput
-                }
-            }
-        }
+function Write-ProgressLog {
+        param(
+            [string]$Activity,
+            [string]$Status
+        )
+        Write-Progress -Activity $Activity -Status $Status -PercentComplete (($currentStep / $totalSteps) * 100)
+        WriteLog $Status
+        $script:currentStep++
+      
     }
-    catch {
-        #$PSCmdlet.ThrowTerminatingError($_)
-        WriteLog $_
-        Write-Host "Script failed - $Logfile for more info"
-        throw $_
-
+Function Get-RemovableDrive {
+writelog "Get information for all removable drives"
+$USBDrives = Get-WmiObject Win32_DiskDrive | Where-Object {$_.MediaType -eq "Removable media"} 
+If($USBDrives -and ($null -eq $USBDrives.count)) {
+        $USBDrivesCount = 1
     }
-    finally {
-        Remove-Item -Path $stdOutTempFile, $stdErrTempFile -Force -ErrorAction Ignore
-		
+    else {
+        $USBDrivesCount = $USBDrives.Count
     }
-	
-}
+    WriteLog "Found $USBDrivesCount USB drives"
 
-function New-DeploymentMedia {
-    param (
-        [Parameter()]
-        [String]$FFUDevelopmentPath = "C:\FFUDevelopment",
-        [Parameter()]
-        [Switch]$Deploy,
-        [String]$WindowsLang = 'en-us'
-    )
-    #Need to use the Demployment and Imaging tools environment to create winPE media
-    $DandIEnv = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
-    $WinPEFFUPath = "$FFUDevelopmentPath\WinPE"
-     $WinPEFFUMedia = "$WinPEFFUPath\Media"
-    If (Test-path -Path "$WinPEFFUPath") {
-        WriteLog "Removing old WinPE path at $WinPEFFUPath"
-        Remove-Item -Path "$WinPEFFUPath" -Recurse -Force | out-null
+    if ($null -eq $USBDrives) {
+        WriteLog "No removable USB drive found. Exiting"
+        Write-Error "No removable USB drive found. Exiting"
+        Pause
+        exit 1
     }
-
-    WriteLog "Copying WinPE files to $WinPEFFUPath" 
-    & cmd /c """$DandIEnv"" && copype amd64 $WinPEFFUPath" | Out-Null
-    WriteLog "Copying tools"
-    Copy-Item -Path "$FFUDevelopmentPath\DriverExporter.exe" -Destination "$WinPEFFUMedia\DriverExporter.exe"
-    Copy-Item -Path "$FFUDevelopmentPath\DriverInstaller.exe" -Destination "$WinPEFFUMedia\DriverInstaller.exe"
-    WriteLog 'Files copied successfully'
-    if($WindowsLang -eq 'en-us'){
-        WriteLog "if default (En-US) Clean up unused language folders from WinPE media folder"
-        $UnusedFolder = Get-ChildItem -Path "$WinPEFFUMedia" -Directory | Where-Object {$_.Name -like "*-*"}
-        foreach($folder in $UnusedFolder){
-        remove-item -Path $folder.fullname -Force -Confirm: $false -Recurse
-        }
-    }else{
-        WriteLog "Cleanup all language folders except $WindowsLang"
-        $UnusedFolder = Get-ChildItem -Path "$WinPEFFUMedia" -Directory | Where-Object {$_.Name -like "*-*" -and $_.Name -notmatch $WindowsLang}
-        foreach($folder in $UnusedFolder){
-        remove-item -Path $folder.fullname -Force -Confirm: $false -Recurse
-        }
-    }
-    Mount-WindowsImage -ImagePath "$WinPEFFUPath\media\sources\boot.wim" -Index 1 -Path "$WinPEFFUPath\mount" | Out-Null
-    WriteLog 'Mounting complete'
-    
-    $Packages = @(
-        "WinPE-WMI.cab",
-        "en-us\WinPE-WMI_en-us.cab",
-        "WinPE-NetFX.cab",
-        "en-us\WinPE-NetFX_en-us.cab",
-        "WinPE-Scripting.cab",
-        "en-us\WinPE-Scripting_en-us.cab",
-        "WinPE-PowerShell.cab",
-        "en-us\WinPE-PowerShell_en-us.cab",
-        "WinPE-StorageWMI.cab",
-        "en-us\WinPE-StorageWMI_en-us.cab",
-        "WinPE-DismCmdlets.cab",
-        "en-us\WinPE-DismCmdlets_en-us.cab"
-    )
-
-    $PackagePathBase = "$adkPath`Assessment and Deployment Kit\Windows Preinstallation Environment\amd64\WinPE_OCs\"
-
-    foreach ($Package in $Packages) {
-        $PackagePath = Join-Path $PackagePathBase $Package
-        WriteLog "Adding Package $Package"
-        Add-WindowsPackage -Path "$WinPEFFUPath\mount" -PackagePath $PackagePath | Out-Null
-        WriteLog "Adding package complete"
- 
+  return $USBDrives, $USBDrivesCount
+  }
   
-        WriteLog "Copying $FFUDevelopmentPath\WinPEDeployFFUFiles\* to WinPE deploy media"
-        Copy-Item -Path "$FFUDevelopmentPath\WinPEDeployFFUFiles\*" -Destination "$WinPEFFUPath\mount" -Recurse -Force | Out-Null
-        WriteLog 'Copy complete'
-        if ($CopyPEDrivers) {
-            WriteLog "Adding drivers to WinPE media"
-            try {
-                Add-WindowsDriver -Path "$WinPEFFUPath\Mount" -Driver "$FFUDevelopmentPath\PEDrivers" -Recurse -ErrorAction SilentlyContinue | Out-null
+Function Build-DeploymentUSB{
+      param(
+            [Array]$Drives       
+            )
+            writelog "Creating list of FFU image files"
+            $Images = Get-ChildItem -Path $FFUPath -Filter "*.ffu" -File -Recurse
+            writelog "Creating list of driver files"
+            $Drivers = Get-ChildItem -Path $DriversPath -Recurse
+            $DrivesCount = $Drives.Count
+            Write-ProgressLog "Create Imaging Tool" "Creating partitions..."
+            writelog "Create job to partition each usb drive"
+            foreach ($USBDrive in $Drives) {
+            $DriveNumber = $USBDrive.DeviceID.Replace("\\.\PHYSICALDRIVE", "")
+            $Model = $USBDrive.model
+            $ScriptBlock = {
+            param($DriveNumber)
+            Clear-Disk -Number $DriveNumber -RemoveData -RemoveOEM -Confirm:$false
+            $Disk = Get-Disk -Number $DriveNumber
+            $PartitionStyle = $Disk.PartitionStyle
+            if($PartitionStyle -ne 'MBR'){
+            $Disk | Set-Disk -PartitionStyle MBR
             }
-            catch {
-                WriteLog 'Some drivers failed to be added to the FFU. This can be expected. Continuing.'
-            }
-            WriteLog "Adding drivers complete"
-                
+            $BootPartition = New-Partition -DiskNumber $DriveNumber -Size 2GB -IsActive -AssignDriveLetter
+            $DeployPartition = New-Partition -DiskNumber $DriveNumber -UseMaximumSize -AssignDriveLetter
+            Format-Volume -Partition $BootPartition -FileSystem FAT32 -NewFileSystemLabel "Boot" -Confirm:$false
+            Format-Volume -Partition $DeployPartition -FileSystem NTFS -NewFileSystemLabel "Deploy" -Confirm:$false
+        }
+        WriteLog 'Start job to create BOOT and Deploy partitions on each drive'
+        Start-Job -ScriptBlock $ScriptBlock -ArgumentList $DriveNumber | Out-Null
+    }
+    writelog "Wait for partitioning jobs to complete"
+    Get-Job | Wait-Job
     
-    WriteLog 'Dismounting WinPE media' 
-    Dismount-WindowsImage -Path "$WinPEFFUPath\mount" -Save | Out-Null
-    WriteLog 'Dismount complete'
-    #Make ISO
-    $OSCDIMGPath = "$adkPath`Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg"
-    $OSCDIMG = "$OSCDIMGPath\oscdimg.exe"
-    WriteLog "Creating WinPE ISO at $FFUDevelopmentPath\$ISOFileName"
-    Invoke-Process $OSCDIMG "-m -o -u2 -udfver102 -bootdata:2`#p0,e,b`"$OSCDIMGPath\etfsboot.com`"`#pEF,e,b`"$OSCDIMGPath\Efisys_noprompt.bin`" `"$WinPEFFUPath\media`" `"$DeployISOPath`""
-    WriteLog "ISO created successfully"
-    WriteLog "Cleaning up $WinPEFFUPath"
-    Remove-Item -Path "$WinPEFFUPath" -Recurse -Force
-    WriteLog 'Cleanup complete'
-    
+    if($DrivesCount -gt 1){
+    writelog "Get file system information for all drives"
+    $Partitions = Get-Partition | Get-Volume
+    }else{
+    writelog "Get file system information for drive number $DiskNumber"
+    $Partitions = Get-Partition -DiskNumber $DriveNumber | Get-Volume
+    }
+writelog "Get drive letter for all volumes labeled:BOOT"
+$BootDrives = ($Partitions | Where-Object { $_.FileSystemLabel -eq "BOOT"}).DriveLetter
+writelog "Get drive letter for all volumes labeled:Deploy"
+$DeployDrives = ($Partitions | Where-Object { $_.FileSystemLabel -eq "Deploy"}).DriveLetter
+writelog "Mount Deployment .iso image"
+$ISOMountPoint = (Mount-DiskImage -ImagePath "$DeployISOPath" -PassThru | Get-Volume).DriveLetter + ":\"
+writelog "Copying boot files to all drives labeled BOOT concurrently"
+foreach ($Drive in $BootDrives) {
+$Destination = $Drive + ":\"
+    $jobScriptBlock = {
+        param (
+            [string]$SFolder,
+            [string]$DFolder
+        )
+        Robocopy $SFolder $DFolder /E /COPYALL /R:5 /W:5 /J
+    }
+    WriteLog 'Start copy job to copy all boot files to each drive'
+    Start-Job -ScriptBlock $jobScriptBlock -ArgumentList $ISOMountPoint, $Destination | Out-Null
+}
+if($Images){
+writelog "Copying FFU image files to all drives labeled Deploy concurrently"
+foreach ($Drive in $DeployDrives) {
+$Destination = $Drive+":\Images"
+    $jobScriptBlock = {
+        param (
+            [string]$SFolder,
+            [string]$DFolder
+        )
+        New-Item -Path $DFolder -ItemType Directory -Force -Confirm: $false | Out-Null
+        Robocopy $SFolder $DFolder /E /COPYALL /R:5 /W:5 /J
+    }
+
+    WriteLog 'Start copy job to copy all FFU files to each drive'
+    Start-Job -ScriptBlock $jobScriptBlock -ArgumentList $ImagesPath, $Destination | Out-Null
+    }
+}
+if(!($Images)){
+    foreach ($Drive in $DeployDrives) {
+        WriteLog "Create images directory"
+        New-Item -Path "$Drive" -Name Images -ItemType Directory -Force -Confirm: $false | Out-Null
+        }
+}
+if($Drivers){
+writelog "Copying driver files to all drives labeled Deploy concurrently"
+foreach ($Drive in $DeployDrives) {
+$Destination = $Drive+":\Drivers"
+    $jobScriptBlock = {
+        param (
+            [string]$SFolder,
+            [string]$DFolder
+        )
+        New-Item -Path $DFolder -ItemType Directory -Force -Confirm: $false | Out-Null
+        Robocopy $SFolder $DFolder /E /COPYALL /R:5 /W:5 /J
+    }
+    WriteLog 'Start copy job to copy all drivers to each drive'
+    Start-Job -ScriptBlock $jobScriptBlock -ArgumentList $DriversPath, $Destination | Out-Null
 }
 }
+if(!($Drivers)){
+   foreach ($Drive in $DeployDrives) {
+        WriteLog "Create images directory"
+        $drivepath = $Drive+":\"
+        New-Item -Path "$drivepath" -Name Drivers -ItemType Directory -Force -Confirm: $false | Out-Null
+        }
 }
-if (Test-Path -Path $Logfile) {
-    Remove-item -Path $LogFile -Force
+if($DrivesCount -gt 1){
+Write-ProgressLog "Create Imaging Tool" "Building $DrivesCount drives concurrently...Please be patient..."
+}else{
+Write-ProgressLog "Create Imaging Tool" "Building the imaging tool on $model...Please be patient..."
+}
+Get-Job | Wait-Job
+Dismount-DiskImage -ImagePath $DeployISOPath | Out-Null
+Write-ProgressLog "Create Imaging Tool" "Drive creation jobs completed..."
 }
 
+Function New-DeploymentUSB {
+    param(
+        [String]$FFUDevelopmentPath = $DevelopmentPath ,
+        [String]$DeployISO =$DeployISOPath,
+        [Array]$Drives,
+        [int]$Count,
+        [String]$FFUPath = "$FFUDevelopmentPath\Images",
+        [String]$DriversPath = "$FFUDevelopmentPath\Drivers"
+        
+    )
+  
+    $Drivelist = @()
+    writelog "Creating a USB drive selection list"
+    for($i=0;$i -le $Count -1;$i++){
+        $DriveModel = $Drives[$i].Model
+        $DriveSize = [math]::round($Drives[$i].size/1GB, 2)
+        $DiskNumber = $Drives[$i].DeviceID.Replace("\\.\PHYSICALDRIVE", "")
+	    $Properties = [ordered]@{Number = $i + 1  ; DriveNumber = $DiskNumber ; DriveModel = $driveModel ; 'Size (GB)' = $DriveSize} 
+        
+        $Drivelist += New-Object PSObject -Property $Properties
+        }
+        if($Count -gt 1){
+        $Last = $Count+1
+        $Drivelist += New-Object -TypeName PSObject -Property @{ Number = "$last"; DriveModel = "Select this option to use all ($count) inserted USB Drives" }       
+        }
+        $Drivelist | Format-Table -AutoSize -Property Number, DriveModel , 'Size (GB)'
+        do {
+        try {
+            $var = $true
+            $DriveSelected = Read-Host 'Enter the drive number to apply the .iso to'
+            $DriveSelected = ($DriveSelected -as [int]) -1
+            writelog "drive $DriveSelected selected"
+            }
+
+        catch {
+            Write-Host 'Input was not in correct format. Please enter a valid FFU number'
+            $var = $false
+        }
+    } until (($DriveSelected -le $Count -1 -or $last) -and $var) 
+    WriteLog "Setting the registry key to disable autoplay for all drives"
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 1 -Type DWORD
+    WriteLog "Closing all MMC windows to prevent drive lock errors"
+    Stop-Process -Name mmc -ErrorAction SilentlyContinue
+    WriteLog "Closing all Diskpart windows to prevent drive lock errors"
+    Stop-Process -Name diskpart -ErrorAction SilentlyContinue
+    $Selection = $Drivelist[$DriveSelected].Number
+    $totalSteps = 5
+    #$startTime = Get-Date
+    if($Selection -eq $last){
+    Read-Host -Prompt "ALL DRIVES SELECTED! WILL ERASE ALL CURRENTLY CONNECTED USB DRIVES!! Press ENTER to continue"
+    Build-DeploymentUSB -Drives $Drives
+    }else{
+    Read-Host -Prompt "Drive number $Selection was selected. Press ENTER to continue"
+    Build-DeploymentUSB -Drives $Drives[$DriveSelected]
+    }
+    WriteLog "Setting the registry key to re-enable autoplay for all drives"
+    Write-ProgressLog "Create Imaging Tool" "Enabling Autoplay"
+    Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 0 -Type DWORD
+    Write-ProgressLog "Create Imaging Tool" "Completed!"
+ }    
+#Get USB Drive and create log file
+if(Test-Path "$DevelopmentPath\Script.log"){
+Remove-Item -Path "$DevelopmentPath\Script.log" -Force -Confirm:$false
+New-item -Path $DevelopmentPath -Name 'Script.log' -ItemType "file" -Force | Out-Null
+}
 WriteLog 'Begin Logging'
-New-DeploymentMedia -Deploy
+WriteLog 'Getting USB drive information and usb drive count'
+$usbDrives,$USBDrivesCount = Get-RemovableDrive
+WriteLog 'Setting first step for percentage progress bar'
+$currentStep = 1 
+New-DeploymentUSB -DeployISO $DeployISOPath -Drives $usbDrives -Count $USBDrivesCount -FFUDevelopmentPath $DevelopmentPath
+#$endTime = Get-Date
+# Calculate duration
+#$duration = $endTime - $startTime
+#Write-Host "Total execution time: $duration"
+read-host -Prompt "USB drive creation complete. Press ENTER to exit"
+WriteLog 'Cleaning up all completed jobs'
+Exit
+}else{
+Write-Host "No .ISO file selected..."
+read-host "Press ENTER to Exit..."
+Exit
+}
